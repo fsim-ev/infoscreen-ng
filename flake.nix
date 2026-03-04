b03fb2a9bdfa3e403335a19f41e345c33de1b7f0
beo45216 <ole.bendixen@st.oth-regensburg.de>
2026-03-04 12:04:24 +0100

    fix noto-fonts in build env

--- flake.nix
+++ flake.nix
@@ -7,33 +7,40 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, crane, flake-utils }:
-     flake-utils.lib.eachDefaultSystem (
-          system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
+  outputs =
+    {
+      self,
+      nixpkgs,
+      crane,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
 
-            inherit (pkgs) lib;
+        inherit (pkgs) lib;
 
-            craneLib = crane.mkLib pkgs;
-            src = craneLib.cleanCargoSource ./.;
+        craneLib = crane.mkLib pkgs;
+        src = craneLib.cleanCargoSource ./.;
 
-            # Common arguments can be set here to avoid repeating them later
-            commonArgs = {
-              inherit src;
-              strictDeps = true;
+        # Common arguments can be set here to avoid repeating them later
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
 
-              buildInputs =  with pkgs; [
-                wayland
-                wayland-protocols
-                # Add additional build inputs here
-              ];
-              propagetedBuildInputs = with pkgs; [
-                wayland
-                wayland-protocols
-                
-              ];
-            };
+          buildInputs = with pkgs; [
+            wayland
+            wayland-protocols
+            noto-fonts-cjk-sans
+            # Add additional build inputs here
+          ];
+          propagetedBuildInputs = with pkgs; [
+            wayland
+            wayland-protocols
+            noto-fonts-cjk-sans
+          ];
+        };
         # Build *just* the cargo dependencies (of the entire workspace),
         # so we can reuse all of that work (e.g. via cachix) when running in CI
         # It is *highly* recommended to use something like cargo-hakari to avoid
@@ -45,19 +52,28 @@
           inherit (craneLib.crateNameFromCargoToml { inherit src; }) version;
           doCheck = false;
         };
-        libPath = with pkgs;
-          lib.makeLibraryPath [ fontconfig libxkbcommon libGL wayland noto-fonts-cjk-sans];
+        libPath =
+          with pkgs;
+          lib.makeLibraryPath [
+            fontconfig
+            libxkbcommon
+            libGL
+            wayland
+            noto-fonts-cjk-sans
+          ];
         wrapper = name: {
-            buildInputs = [pkgs.makeWrapper ];
-            postInstall = ''
-                wrapProgram $out/bin/${name} \
-                  --prefix LD_LIBRARY_PATH : ${libPath}
-              '';
-          
+          buildInputs = [ pkgs.makeWrapper ];
+          postInstall = ''
+            wrapProgram $out/bin/${name} \
+              --prefix LD_LIBRARY_PATH : ${libPath}
+          '';
+
         };
 
         infoscreen-bus-schedule = craneLib.buildPackage (
-          individualCrateArgs  // (wrapper "infoscreen-bus-schedule") // rec {
+          individualCrateArgs
+          // (wrapper "infoscreen-bus-schedule")
+          // rec {
             pname = "infoscreen-bus-schedule";
             cargoExtraArgs = "-p infoscreen-bus-schedule --all-features";
             src = ./.;
@@ -65,7 +81,9 @@
           }
         );
         infoscreen-todo-list = craneLib.buildPackage (
-          individualCrateArgs  // (wrapper "infoscreen-todo-list") // rec {
+          individualCrateArgs
+          // (wrapper "infoscreen-todo-list")
+          // rec {
             pname = "infoscreen-todo-list";
             cargoExtraArgs = "-p infoscreen-todo-list --all-features";
             src = ./.;
@@ -73,33 +91,37 @@
           }
         );
         infoscreen-timetable = craneLib.buildPackage (
-          individualCrateArgs // (wrapper "infoscreen-timetable") // rec {
+          individualCrateArgs
+          // (wrapper "infoscreen-timetable")
+          // rec {
             pname = "infoscreen-timetable";
             cargoExtraArgs = "--all-features";
             src = ./.;
             meta.mainProgram = pname;
           }
         );
-    in {
-      devShells.default = craneLib.devShell {
-        nativeBuildInputs = with pkgs; [
-          rustc
-          cargo
-          clippy
-          # openssl
-          # pkg-config
-          rust-analyzer
-          rustfmt # formatter
-        ];
+      in
+      {
+        devShells.default = craneLib.devShell {
+          nativeBuildInputs = with pkgs; [
+            rustc
+            cargo
+            clippy
+            # openssl
+            # pkg-config
+            rust-analyzer
+            rustfmt # formatter
+          ];
 
-        LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${libPath}";
+          LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${libPath}";
 
-        # uncomment this is you get some kind of ssl error, usually on anything networking related using reqwest
-        # PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-      };
+          # uncomment this is you get some kind of ssl error, usually on anything networking related using reqwest
+          # PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+        };
 
-      packages = {
-        inherit infoscreen-bus-schedule infoscreen-todo-list infoscreen-timetable;
-      };
-    });
+        packages = {
+          inherit infoscreen-bus-schedule infoscreen-todo-list infoscreen-timetable;
+        };
+      }
+    );
 }
